8b3f4eb8074c4eb6354d3a6f3de56038c94ad570
Sardor5544 <sardor_zv@inbox.ru>
2025-05-14 16:04:55 +0500

    Update 1.sql

--- lesson-1/homework/1.sql
+++ lesson-1/homework/1.sql
@@ -1,6 +1,10 @@
+create database [SchoolDB]
+
 create table Students
     student_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     Age INT
 
-
+-- SQL Server - it is the "brain" that saves and processes data
+-- SSMS - it is "interface" due to working with this "brain"
+-- SQL - it is "language" on which we speak with server
